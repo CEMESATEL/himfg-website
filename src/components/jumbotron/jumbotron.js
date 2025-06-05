fdb779b9e5c82384ca4b425eff34421bd0cdbd30
CEMESATEL <cemesatel@himfg.edu.mx>
2025-06-05 08:16:19 -0600

    [delete]: se elimina el banner del open house

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -3,46 +3,42 @@ import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick'
 import styles from './jumbotron.module.css'
 
-export const Jumbotron =()=>{
-        const settings = {
-            dots: true,
-            infinite: true,
-            speed: 1000,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows:false,
-            autoplay:true,
-            speed: 2000,
-            autoplaySpeed: 3000,
-        }
-        const images = [
+export const Jumbotron = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 3000,
+    }
+    const images = [
+        {
+            url: 'https://himfg.edu.mx/archivos/banners/banner-segundo.webp',
+            link: 'https://ense.himfg.edu.mx/convocatoria/documentos/convocatoria.pdf'
+        },
+    ]
+    return (
+        <Slider {...settings}>
             {
-                url:'https://himfg.edu.mx/archivos/banners/banner-primero.webp',
-                link:'/galeria'
-            },
-            {
-                url:'https://himfg.edu.mx/archivos/banners/banner-segundo.webp',
-                link:'https://ense.himfg.edu.mx/convocatoria/documentos/convocatoria.pdf'
-            },
-        ]
-          return (
-            <Slider {...settings}>
-                {
-                    images.map((image, index)=>(
-                        <div
+                images.map((image, index) => (
+                    <div
                         key={index}
                         className={styles.jumbotronWrapper}
-                        >
-                            <a href={`${image.link}`}>
-                                <img
-                                    src={image.url}
-                                    alt={`Imagen`}
-                                    className={styles.jumbotronImage}
-                                />
-                            </a>
-                        </div>
-                    ))
-                }
-            </Slider>
-          );
+                    >
+                        <a href={`${image.link}`}>
+                            <img
+                                src={image.url}
+                                alt={`Imagen`}
+                                className={styles.jumbotronImage}
+                            />
+                        </a>
+                    </div>
+                ))
+            }
+        </Slider>
+    );
 }
